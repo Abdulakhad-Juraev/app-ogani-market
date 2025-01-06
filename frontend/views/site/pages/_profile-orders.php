ae8a2423c137427e813e2f9aff483a8bd0d4233e
Abdulakhad <computerstatuff@gmail.com>
2025-01-07 00:12:01 +0500

    commit feat: profile order table custom

--- frontend/views/site/pages/_profile-orders.php
+++ frontend/views/site/pages/_profile-orders.php
@@ -6,117 +6,9 @@ use yii\helpers\Url;
 
 /** @var Order $orders */
 ?>
-
-<div class="wrapper">
-    <!-- Main Sidebar Container -->
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper ml-0">
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <!-- Main content -->
-                        <div class="invoice p-3 mb-3">
-                            <!-- title row -->
-                            <div class="row">
-                                <div class="col-12">
-                                    <h4>
-                                        <i class="fas fa-globe"></i> Online market,
-                                        Inc.
-                                        <small class="float-right">Date:
-                                            <?= Yii::$app->formatter->asDatetime($orders[0]->created_at, 'php:Y-m-d') ?? '' ?>
-                                        </small>
-                                    </h4>
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- Table row -->
-                            <div class="row">
-                                <div class="col-12 table-responsive">
-                                    <table class="table table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th>Mahsulot</th>
-                                            <th>Soni</th>
-                                            <th>Narxi</th>
-                                            <th>Summa</th>
-                                            <th>Tolov turi</th>
-                                            <th>Comment</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php $all_sum = 0; ?>
-                                        <?php foreach ($orders as $order): ?>
-                                            <?php foreach ($order->orderItems as $item): ?>
-                                                <tr>
-                                                    <td><?= $item->product->name ?? '' ?></td>
-                                                    <td><?= $item->count ?? '' ?></td>
-                                                    <td><?= $item->price ?? '' ?></td>
-                                                    <td><?= $item->total_price ?? '' ?></td>
-                                                    <td><?= $order->paymentTypeName ?? '' ?></td>
-                                                    <th>
-                                                        <a href="<?= Url::to(['/site/profile-update', 'product_id' => $item->product->id]) ?>"
-                                                           class="btn badge badge-warning comment-btn"
-                                                           data-product-name="<?= $item->product->name ?? '' ?>"><i
-                                                                    class="far fa-comments"></i></a>
-                                                </tr>
-                                            <?php endforeach; ?>
-                                            <?php $all_sum += $order->allPrice; ?>
-                                            <tr>
-                                                <td><b>Zakaz id <?= $order->id ?? '' ?> </b></td>
-                                                <td></td>
-                                                <td>
-                                                    <b><?= Yii::$app->formatter->asDatetime($order->created_at, 'php:Y-m-d') ?? '' ?></b>
-                                                </td>
-                                                <td><b>Summa</b></td>
-                                                <th></th>
-                                                <td>
-                                                    <b><?= $order->allPrice ?? '' ?></b>
-                                                </td>
-                                            </tr>
-                                        <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
-
-                            <div class="row mt-3">
-                                <!-- accepted payments column -->
-                                <div class="col-6">
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-6">
-                                    <div class="table-responsive">
-                                        <table class="table">
-                                            <tbody>
-                                            <tr>
-                                                <th>Total:</th>
-                                                <td><b><?= $all_sum ?? ''; ?></b>
-                                                </td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                        <!-- /.invoice -->
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-    <div id="sidebar-overlay"></div>
-</div>
 <div class="row">
     <div class="col-md-12">
-        <div class="card">
+        <div class="card card-success card-outline">
             <div class="card-header">
                 <h4 class="card-title w-100 d-flex justify-content-between align-items-center" style="font-size:23px;">
                         <span>
@@ -186,15 +78,13 @@ use yii\helpers\Url;
                         <?php $all_sum += $order->allPrice; ?>
 
                     <?php endforeach; ?>
+
                     </tbody>
                     <tfoot>
                     <tr>
-                        <td colspan="3">
-                    <tr>
-                        <th>Total:</th>
-                        <td><b><?= $all_sum ?? ''; ?></b></td>
-                    </tr>
-                    </td>
+                        <td colspan="5"></td>
+                        <td class="pt-5 pb-3">Total:</td>
+                        <td colspan="2" class="pt-5 pb-3"><b><u><?= $all_sum ?? ''; ?></u></b></td>
                     </tr>
                     </tfoot>
                 </table>
